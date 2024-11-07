2e6597326fe507741060b5b73352f32d8437f7d6
Brendon Butler <brendonaaron97@gmail.com>
2024-11-07 15:19:30 -0500

    dynamic api route to pull a random dad joke from icanhazdadjoke minor cleanup

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,12 @@ import path from 'path';
 import fs from 'fs/promises';
 import url from 'url';
 
+interface Joke {
+  id: string;
+  joke: string;
+  status: number;
+}
+
 async function requestListener(req: http.IncomingMessage, res: http.ServerResponse) {
   const parsedUrl = url.parse(req.url || '', true);
 
@@ -12,16 +18,28 @@ async function requestListener(req: http.IncomingMessage, res: http.ServerRespon
   try {
     let pathName = parsedUrl.pathname;
 
-    if (pathName === "/") pathName = '/index';
+    if (pathName === '/') pathName = '/index';
 
     const filePath = path.join(__dirname, `static${pathName}.html`);
-    data = await fs.readFile(filePath, "utf-8");
+    data = await fs.readFile(filePath, 'utf-8');
   } catch {
-    data = await fs.readFile(path.join(__dirname, 'static/404.html'), "utf-8");
+    data = await fs.readFile(path.join(__dirname, 'static/404.html'), 'utf-8');
     statusCode = 404;
   }
+  
+  if (parsedUrl.pathname === '/dad-joke') {
+    const response = await fetch('https://icanhazdadjoke.com/', {
+      headers: {
+        accept: 'application/json',
+        'user-agent': 'NodeJS Server'
+      }
+    });
 
-  res.writeHead(statusCode, { 'Content-Type': 'text/html', "content-length": data.length });
+    const joke: Joke = await response.json();
+    data = data.replace(/{{joke}}/gm, joke.joke);
+  }
+
+  res.writeHead(statusCode, { 'Content-Type': 'text/html', 'content-length': data.length });
   res.write(data);
   res.end();
 }
